1359e03403ee94d3b78c2581cc7d5aa578328894
roberto <rjsdnrsla@naver.com>
2021-02-20 23:27:01 +0900

    card scroll_ plus rihgt_Button

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 const backToTop = document.getElementById("backtotop");
 
 function checkScroll() {
-  const pageLength = window.pageYOffset;
+    const pageLength = window.pageYOffset;
 
-  if (pageLength !== 0) {
-    backToTop.classList.add("show");
-  } else {
-    backToTop.classList.remove("show");
-  }
+    if (pageLength !== 0) {
+        backToTop.classList.add("show");
+    } else {
+        backToTop.classList.remove("show");
+    }
 }
 
 function moveBackToTop() {
-  if (window.pageYOffset > 0) {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
+    if (window.pageYOffset > 0) {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 }
 
 window.addEventListener("scroll", checkScroll);
@@ -21,55 +21,100 @@ backToTop.addEventListener("click", moveBackToTop);
 
 
 /*=============== card scroll ==================*/
-function transformLeft(event){
-    const slideLeft =event.target;
-    const slideRight =slideLeft.nextElementSibling;
+function transformRight(event) {
+    const slideRight = event.target;
+    const slideLeft = slideRight.previousElementSibling;
 
     /*ul*/
-    const classList=slideLeft.parentElement.parentElement.nextElementSibling;
+    const classList = slideRight.parentElement.parentElement.nextElementSibling;
+    let activeLi = classList.getAttribute('data-position');
+    const liList = classList.getElementsByTagName('li');
+
+    if (Number(activeLi) < 0) {
+        /*오른쪽 이동*/
+        activeLi = Number(activeLi) + 260;
+
+        /*오른쪽이동 했으니 왼쪽버튼활성화*/
+        slideLeft.style.color = '#2f3059';
+        slideLeft.classList.add('slide-left-hover');
+        slideLeft.addEventListener('click', transformLeft);
+
+        /*카트 위치 왼쪽 맨끝일떄  오른쪽버튼 비활성*/
+        if(Number(activeLi)===0){
+            slideRight.style.color = '#cfd8dc';
+            slideRight.classList.remove('slide-right-hover');
+            /* 오른쪽 버튼 설정*/
+            slideRight.removeEventListener('click', transformRight);
+        }         
+    }
+
+    /*위 조건에따른 카드(ul) 이동 */
+    classList.style.transition = 'transform 1s';
+    classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+    /*data-position 에 바뀐 위치값 저장*/
+    classList.setAttribute('data-position', activeLi);
+
+
+}
+
+
+
+function transformLeft(event) {
+    const slideLeft = event.target;
+    const slideRight = slideLeft.nextElementSibling;
+
+    /*ul*/
+    const classList = slideLeft.parentElement.parentElement.nextElementSibling;
     let activeLi = classList.getAttribute('data-position');
     const liList = classList.getElementsByTagName('li');
 
     /*오른쪽에 카드가 남아있을때  -> 왼쪽버튼 활성*/
-    if(classList.clientWidth < (liList.length * 260 + Number(activeLi))){
-        activeLi =Number(activeLi)-260;
+    if (classList.clientWidth < (liList.length * 260 + Number(activeLi))) {
+        /*왼쪽으로 밀어준다*/
+        activeLi = Number(activeLi) - 260;
 
-         /* 카드가  ul 공간보다 없을때  -> 버튼비활성*/
-        if(classList.clientWidth > (liList.length * 260 + Number(activeLi))){
-        slideLeft.style.color='#cfd8dc';
-        slideLeft.classList.remove('slide-left-hover');
+        /* 전체카드(ul)길이가  ul 공간보다 없을때  -> 왼쪽 버튼비활성*/
+        if (classList.clientWidth > (liList.length * 260 + Number(activeLi))) {
+            slideLeft.style.color = '#cfd8dc';
+            slideLeft.classList.remove('slide-left-hover');
+            /* 오른쪽 버튼 설정*/
+            slideLeft.removeEventListener('click', transformLeft);
         }
 
-        slideRight.style.color='#2f3059';
+        /*왼쪽으로 밀어준뒤 -> 오른쪽 버튼 활성*/
+        slideRight.style.color = '#2f3059';
         slideRight.classList.add('slide-right-hover');
+        slideRight.addEventListener('click', transformRight);
     }
 
+    /*위 조건에따른 카드(ul) 이동 */
     classList.style.transition = 'transform 1s';
-    classList.style.transform  = 'translateX(' + String(activeLi) +'px)';
-    classList.setAttribute('data-position',activeLi);
+    classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+    /*data-position 에 바뀐 위치값 저장*/
+    classList.setAttribute('data-position', activeLi);
 
 }
 
 
 const slideLeftList = document.getElementsByClassName("slide-left");
 
-for(let i=0; i<slideLeftList.length; i++){
+for (let i = 0; i < slideLeftList.length; i++) {
     let classList = slideLeftList[i].parentElement.parentElement.nextElementSibling;
     /*li*/
     let liList = classList.getElementsByTagName("li");
 
     /*ul너비  < li개수 * li.width*/
-    if(classList.clientWidth <(liList.length * 260)){
+    if (classList.clientWidth < (liList.length * 260)) {
         /*left 버튼 활성화*/
         slideLeftList[i].classList.add("slide-left-hover");
-        slideLeftList[i].addEventListener("click",transformLeft);
-    }else{
+        slideLeftList[i].addEventListener("click", transformLeft);
+    } else {
         /* 버튼 버튼비활성화*/
-        const arrowContainer=slideLeftList[i].parentElement;
+        const arrowContainer = slideLeftList[i].parentElement;
         arrowContainer.removeChild(slideLeftList[i].nextElementSibling);
-       arrowContainer.removeChild(slideLeftList[i]);
+        arrowContainer.removeChild(slideLeftList[i]);
         // arraowContainer.removeChild(slideLeftList[i]);
-    
+
     }
 }
 
