5102365f6e6b2c905783f6ab5b65c5e41fc1cef4
roberto <rjsdnrsla@naver.com>
2021-03-01 19:26:43 +0900

    404page updata

--- js/main.js
+++ js/main.js
@@ -1,11 +1,4 @@
-/*=============== sidebar ==================*/
 
-function toggleMenu() {
-  let navigation = document.querySelector(".navigation");
-  let toggle = document.querySelector(".toggle");
-  navigation.classList.toggle("active");
-  toggle.classList.toggle("active");
-}
 
 /*=============== backToTop  ==================*/
 
